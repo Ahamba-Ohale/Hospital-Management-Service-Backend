d4567f27ca142a397fbb67980a82556907e24518
marcuscharles-code <charlesfatunsin@gmail.com>
2024-02-21 15:59:48 +0100

    server fix

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
 require('dotenv').config();
 
-const cookieParser = require('cookie-parser');
 
+const userRoute = require("./routes/userRoute")
+const cookieParser = require('cookie-parser');
+const articleRoute = require('./routes/articleRoutes');
+const doctorRoutes = require('./route/admin/doctors')
+const departmentRoutes = require('./route/admin/departments')
+const patientRoutes = require('./route/admin/patients')
+const nurseRoutes = require('./route/admin/nurses')
+const pharmacyRoutes = require('./route/admin/pharmacy')
+const uploadRoutes = require('./routes/uploadRoute')
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-
+const bodyParser = require('body-parser')
 const router = require('./routes/paymentRoute');
-
+const paystack = require('paystack')(process.env.PAYSTACK_SECRET_KEY);
 
 
 const bodyParser = require('body-parser')
 // const paystack = require('paystack')(process.env.PAYSTACK_SECRET_KEY);
 const userRoutes = require('./routes/userRoute');
-
 
 
 // express App
@@ -22,11 +29,13 @@ const app = express();
 app.use(express.json());
 
 app.use(cors());
-
+app.use(bodyParser.json()); // to support JSON-encoded bodies
 app.use(express.json()); // to support JSON-encoded bodies
 console.log('Middleware setup complete');
 app.use(router);
 console.log('router complete');
+app.use(cookieParser());
+
 
 
 // middleware
@@ -37,7 +46,14 @@ app.use((req, res, next) => {
 })
 
 
-
+// routes
+app.use('/api/articleRoutes', articleRoute)
+app.use('/api/doctors', doctorRoutes)
+app.use('/api/departments', departmentRoutes)
+app.use('/api/patients', patientRoutes)
+app.use('/api/nurses', nurseRoutes)
+app.use('/api/pharmacy', pharmacyRoutes)
+app.use('/api/uploadRoute', uploadRoutes)
 app.use('/api/', userRoutes);
 
 //connect to db
